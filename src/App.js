91cccab364dd4ce94f7c38ac0693dba1a4ecaef1
Daniel Schrader <dhs@schrader-wolff-media.de>
2022-03-04 19:33:35 +0100

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Article from './components/Article';
 import AddArticle from './components/AddArticle';
-import DataBase from './DataBase';
+// import DataBase from './DataBase';
 
 export default function App() {
-  const [articles, setArticles] = useState(DataBase);
+  const [articles, setArticles] = useState([]);
+  const [apiURL, setApiURL] = useState(
+    'https://fetch-me.vercel.app/api/shopping/items'
+  );
+  useEffect(() => {
+    importArticles();
+    async function importArticles() {
+      try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        setArticles(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [apiURL]);
 
   function handleRemoveArticle(articleId) {
     setArticles(articles.filter((article) => article._id !== articleId));
